be3477b694239cbe25479385dc75c479f7701e62
Neeti <neeti.mongoosetech@gmail.com>
2026-02-12 12:52:59 +0530

    update

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,11 @@ async function bootstrap() {
     }),
   );
   app.enableCors({
-    origin: ['http://localhost:5173', 'https://mini-mu-liard.vercel.app'],
+    origin: [
+      'http://localhost:5173',
+      'https://mini-mu-liard.vercel.app',
+      'https://mini-v3ae-9qha245u7-neetis-projects-55f0d5f4.vercel.app/',
+    ],
 
     methods: 'GET, HEAD, PUT, POST, DELETE, PATCH',
     credentials: true,
